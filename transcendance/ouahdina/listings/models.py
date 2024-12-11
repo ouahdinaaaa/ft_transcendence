ab4d3c927874673e05babe59258613e6998ca194
ouahdinaaaa <a.ouahdina1639@gmail.com>
2024-12-11 23:09:32 +0100

    new push

---
+++ transcendance/ouahdina/listings/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Band(models.Model):
+    name = models.fields.CharField(max_length=100)
+
+# Create your models here.
---
+++ transcendance/ouahdina/listings/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from listings.models import Band
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+        </ul>
+""")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons la vie !</p>')
