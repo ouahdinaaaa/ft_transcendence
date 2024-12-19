5dc2465f5d18628eea53851722c7046d67d3c542
Kamel BOUZEGHOUB <kamelbouzeghoubusma@gmail.com>
2024-12-19 18:48:18 +0100

    Nouvelles bases

--- transcendance/ouahdina/users/form.py
+++
@@ -1,7 +0,0 @@
-# change
-from django.contrib.auth.models import User
-
-# to
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
--- transcendance/ouahdina/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Route de base pour /users/
-    path('register/', views.register),
-    path('login/', views.login_view),
-]
